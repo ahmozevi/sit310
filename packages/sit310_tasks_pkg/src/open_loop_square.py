3b83286c27cc6bda2a1b42784b071d196441f44f
safwan <safwan@SIT310.myguest.virtualbox.org>
2026-05-11 21:31:26 +1000

    Add ROS open loop square movement script

--- packages/sit310_tasks_pkg/src/open_loop_square.py
+++ packages/sit310_tasks_pkg/src/open_loop_square.py
@@ -1,37 +1,60 @@
 #!/usr/bin/env python3
 
+import rospy
 import time
+from duckietown_msgs.msg import WheelsCmdStamped
 
 
-def move_forward(duration):
-    print("Moving forward for", duration, "seconds")
-    time.sleep(duration)
+class OpenLoopSquare:
+    def __init__(self):
+        rospy.init_node("open_loop_square_node", anonymous=True)
 
+        self.pub = rospy.Publisher(
+            "/deakinbot/wheels_driver_node/wheels_cmd",
+            WheelsCmdStamped,
+            queue_size=1
+        )
 
-def turn_left(duration):
-    print("Turning left for", duration, "seconds")
-    time.sleep(duration)
+        rospy.sleep(2)
 
+    def set_wheels(self, left, right, duration):
+        msg = WheelsCmdStamped()
+        msg.vel_left = left
+        msg.vel_right = right
 
-def stop_robot():
-    print("Stopping robot")
-    time.sleep(1)
+        start_time = time.time()
 
+        while time.time() - start_time < duration and not rospy.is_shutdown():
+            msg.header.stamp = rospy.Time.now()
+            self.pub.publish(msg)
+            rospy.sleep(0.1)
 
-def main():
-    forward_time = 2.0
-    turn_time = 1.0
+    def stop(self):
+        self.set_wheels(0.0, 0.0, 1.0)
 
-    print("Starting open loop square movement")
+    def run_square(self):
+        forward_time = 2.0
+        turn_time = 1.0
 
-    for side in range(4):
-        print("Side", side + 1)
-        move_forward(forward_time)
-        turn_left(turn_time)
+        forward_speed = 0.3
+        turn_speed = 0.25
 
-    stop_robot()
-    print("Open loop square movement finished")
+        rospy.loginfo("Starting open loop square movement")
+
+        for side in range(4):
+            rospy.loginfo("Moving forward")
+            self.set_wheels(forward_speed, forward_speed, forward_time)
+
+            rospy.loginfo("Turning left")
+            self.set_wheels(-turn_speed, turn_speed, turn_time)
+
+        self.stop()
+        rospy.loginfo("Open loop square movement finished")
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        controller = OpenLoopSquare()
+        controller.run_square()
+    except rospy.ROSInterruptException:
+        pass
